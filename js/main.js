00a90fd248987407df542d0cf2d3029c3e9b117f
moringa <tressy.mbone@student.moringaschool.com>
2021-12-02 11:53:25 +0300

    initial changes

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+  
+function TotalPizza(namePizza,nameCrust,nameTopping){
+    this.OwnPizza=namePizza;
+    this.OwnCrust=nameCrust; 
+    this.OwnTopping=nameTopping;
+  }
+  
+  TotalPizza.prototype.myOwnTopping=function(){
+  
+      return this.OwnTopping
+  }
+
+  $(document).ready(function() {
+  
+  
+   
+        $(".locationOrder").on("click",function(e){
+            e.preventDefault()
+            var name = $("#fname").val();
+            var email = $("#mail").val();
+            var location = $("#txt").val();
+            if (!name || !email || !location) {
+    
+                alert("Fill in all the values")
+    
+            } else {
+              
+                alert(` ${name} Your order will be delivered to ${location} and pay 500ksh extra`)
+    
+          
+                name = $("#fname").val("");
+                email = $("#mail").val("");
+                location = $("#txt").val("");
+    
+    
+            }
+        })
+
+        
+  
+      $("#checkbtn").click(function(){
+  
+        var pizzaSize=$("#myPizzaSize option:selected").text() 
+        var pizzaCrust=$("#myPizzaCrust option:selected").text() 
+        var pizzaTopping=$("#myPizzaTopping option:selected").text() 
+ 
+        var pizzaNumbers=parseInt($("#myPizzaSize option:selected").val() )
+        var crustNumbers=parseInt($("#myPizzaCrust option:selected").val() )
+        var toppingNumbers=parseInt($("#myPizzaTopping option:selected").val()) 
+ 
+        var crustInputNumbers=parseInt($("#crustNumber").val()) 
+        var toppingInputNumbers=parseInt($("#toppingNumber").val()) 
+        var pizzaInputNumbers=parseInt($("#pizzaNumber").val()) 
+    
+ 
+     var myObject=new TotalPizza(pizzaSize,pizzaCrust,pizzaTopping)
+     var Total=(pizzaNumbers + (crustNumbers*crustInputNumbers)+ (toppingNumbers*toppingInputNumbers))*pizzaInputNumbers;
+ 
+   
+ $(".summary").html("<h2>"+"Pizza Size: "+myObject.OwnPizza+"<br>"+"Crust Type: "+myObject.OwnCrust+"<br>"+"Pizza Topping: "+myObject.myOwnTopping()+"<br>"+"Total Amount:"+"kshs."+Total+"</h2>").show(2000)
+ 
+ 
+     })
+   
+   
+ 
+ })
+          
